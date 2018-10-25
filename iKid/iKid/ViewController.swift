ff8e3faf7727ee26599f1caa95795edfd75fb0e4
Nathan Han <nh0713@uw.edu>
2018-10-25 13:20:53 -0700

    test upload

---
+++ iKid/iKid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iKid
+//
+//  Created by Nathan Han on 10/25/18.
+//  Copyright © 2018 Nathan Han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
